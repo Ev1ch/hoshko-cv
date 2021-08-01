a53a69e9acbcdce9d1aa2e3b3dd6922620e5dab0
Vlad Zusko <vladzvu11@gmail.com>
2021-08-01 17:07:24 +0300

    Connected navigation to index file

--- scripts/index.mjs
+++ scripts/index.mjs
@@ -1,3 +1,8 @@
 import { hideLoader } from './components/preloader.mjs';
+import Navigation from './components/navigation.mjs';
+import NAVIGATION_OPTIONS from './constants/navigation-options.mjs';
 
-window.addEventListener('load', hideLoader);
+window.addEventListener('load', () => {
+  hideLoader();
+  const navigation = new Navigation(NAVIGATION_OPTIONS);
+});
