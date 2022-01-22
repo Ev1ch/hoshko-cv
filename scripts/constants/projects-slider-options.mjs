fd34d0d8eef36374b8b2a43ffead8ff07b814f11
Vlad Zusko <vladzvu11@gmail.com>
2022-01-22 13:59:55 +0200

    Added slider scripts

---
+++ scripts/constants/projects-slider-options.mjs
@@ -0,0 +1,18 @@
+export const PROJECTS_SLIDER_OPTIONS = {
+  pagination: {
+    el: '.projects-slider__pagination',
+    bulletClass: 'projects-slider__bullet',
+    bulletActiveClass: 'projects-slider__bullet--active',
+  },
+  centeredSlides: true,
+  slideClass: 'project',
+  spaceBetween: 300,
+  slidesPerView: 'auto',
+  navigation: {
+    disabledClass: 'projects-slider__arrow--disabled',
+    nextEl: '.projects-slider__arrow--right',
+    prevEl: '.projects-slider__arrow--left',
+  },
+};
+
+export const PROJECTS_SLIDER_ROOT = '.projects-slider';
--- scripts/index.mjs
+++ scripts/index.mjs
@@ -1,8 +1,17 @@
 import { hideLoader } from './components/preloader.mjs';
 import Navigation from './components/navigation.mjs';
 import NAVIGATION_OPTIONS from './constants/navigation-options.mjs';
+import {
+  PROJECTS_SLIDER_ROOT,
+  PROJECTS_SLIDER_OPTIONS,
+} from './constants/projects-slider-options.mjs';
 
 window.addEventListener('load', () => {
   hideLoader();
+
   const navigation = new Navigation(NAVIGATION_OPTIONS);
+  const projectsSlider = new Swiper(
+    PROJECTS_SLIDER_ROOT,
+    PROJECTS_SLIDER_OPTIONS,
+  );
 });
